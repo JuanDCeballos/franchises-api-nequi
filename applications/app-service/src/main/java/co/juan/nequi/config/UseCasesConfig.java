33f4fd6f41ef99cbbcb1defc4bad70e07e53dc6a
Juan Ceballos <juandavi789@hotmail.com>
2025-10-10 09:43:40 -0500

    Initial setup

---
+++ applications/app-service/src/main/java/co/juan/nequi/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.juan.nequi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.juan.nequi.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
