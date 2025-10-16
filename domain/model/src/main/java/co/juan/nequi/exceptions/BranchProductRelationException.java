9c27fc2806732e7ebcc4a98fa9b378425f381d4f
Juan Ceballos <juandavi789@hotmail.com>
2025-10-15 23:12:17 -0500

    Deleted unnecesary exceptions

--- domain/model/src/main/java/co/juan/nequi/exceptions/BranchProductRelationException.java
+++
@@ -1,7 +0,0 @@
-package co.juan.nequi.exceptions;
-
-public class BranchProductRelationException extends RuntimeException {
-    public BranchProductRelationException(Long idProduct, Long idBranch) {
-        super("Product with id: " + idProduct + " does not belong to branch with id: " + idBranch);
-    }
-}
